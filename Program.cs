c9748ea50e99c4ec3addaae66233728e7d61bf5a
faranasj <faranasj@gmail.com>
2023-10-21 07:08:22 +0100

    Method added to open excel files

--- Program.cs
+++ Program.cs
@@ -34,6 +34,32 @@ namespace ExcelEditorApp
                     workbookPart.Workbook.Save();
                 }
             }
+
+            using (SpreadsheetDocument doc = SpreadsheetDocument.Open(filePath, false))
+            {
+                WorkbookPart workbookPart = doc.WorkbookPart;
+                Sheet sheet = workbookPart.Workbook.Descendants<Sheet>().First();
+                WorksheetPart worksheetPart = (WorksheetPart)workbookPart.GetPartById(sheet.Id);
+                SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+
+                if (!sheetData.Elements<Row>().Any())
+                {
+                    Console.WriteLine("Current Excel file is empty!");
+                }
+                else
+                {
+                    Console.WriteLine("This is the current content of your file");
+
+                    foreach (Row row in sheetData.Elements<Row>())
+                    {
+                        foreach (Cell cell in row.Elements<Cell>())
+                        {
+                            Console.Write(cell.CellValue.Text + "\t");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
         }
     }
 }
