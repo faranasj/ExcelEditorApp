be058ccb3c04ef75bd862b90fe6b3f1a2e5feef4
faranasj <faranasj@gmail.com>
2023-10-21 06:48:59 +0100

    Added a method to create a new excel file

--- Program.cs
+++ Program.cs
@@ -1,10 +1,39 @@
-﻿namespace ExcelEditorApp
+﻿using System;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+
+namespace ExcelEditorApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Excel File Editor App!");
+            Console.WriteLine("Please provide the filename of your excel file (e.g. your-file.xlsx).");
+            Console.WriteLine("Disclaimer - If no filename is given, 'your-file.xlsx' is used. Additionally, if file does not exist, a new one is created.");
+            string filePath = Console.ReadLine()!;
+
+            if (filePath == string.Empty)
+            {
+                filePath = "your-file.xlsx";
+            }
+
+            if (!File.Exists(filePath))
+            {
+                using (SpreadsheetDocument doc = SpreadsheetDocument.Create(filePath, SpreadsheetDocumentType.Workbook))
+                {
+                    WorkbookPart workbookPart = doc.AddWorkbookPart();
+                    workbookPart.Workbook = new Workbook();
+                    WorksheetPart worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+                    worksheetPart.Worksheet = new Worksheet(new SheetData());
+                    Sheets sheets = doc.WorkbookPart.Workbook.AppendChild(new Sheets());
+                    Sheet sheet = new Sheet() { Id = doc.WorkbookPart.GetIdOfPart(worksheetPart), SheetId = 1, Name = "Sheet1" };
+                    sheets.Append(sheet);
+                    workbookPart.Workbook.Save();
+                }
+            }
         }
     }
 }
